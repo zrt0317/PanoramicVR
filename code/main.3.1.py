e03e49986041545c25a401af9d5577d72d0b0849
zrt0317 <zrt0317@pku.edu.cn>
2019-06-09 21:51:49 +0800

    Update main.3.1.py

--- code/main.3.1.py
+++ code/main.3.1.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import numpy as np
 
 
 filename1 = 'P1b.png'
@@ -23,3 +24,21 @@ matches = sorted(matches, key = lambda x:x.distance)
 matchImg = cv2.drawMatches(p1, kp1, p2, kp2, matches[:52], p1, 
         matchColor=(255,0,0), singlePointColor=(255,0,0), flags=2)
 cv2.imwrite(str(int(time.time()))+'.jpg', matchImg)
+
+
+# Initialize lists
+mylst, dif = [], []
+# For each match...
+for mat in matches:
+    # Get the matching keypoints for each of the images
+    img1_idx = mat.queryIdx
+    img2_idx = mat.trainIdx
+    # x - columns; y - rows; Get the coordinates
+    (x1,y1) = kp1[img1_idx].pt
+    (x2,y2) = kp2[img2_idx].pt
+    # Append to list
+    mylst.append([x1, y1, x2, y2])
+    dif.append([x1-x2, y1-y2])
+
+mylst = np.round(mylst, 0)
+dif = np.round(dif, 0)
